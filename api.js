024c4444b0e5df92c0f330f093c4278650139077
mojosyscom-lang <mojosyscom@gmail.com>
2026-02-02 02:39:13 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const API="PASTE_APPS_SCRIPT_URL_HERE";
+const API="https://script.google.com/macros/s/AKfycbxaSy-VKLITAJQnKC1GHjPTJV3agav3XFs5uKLP-Btw360jm5C6AZs0FRpa9P3XL_iMsg/exec";
 
 async function api(data){
   const r=await fetch(API,{method:"POST",body:JSON.stringify(data)});
