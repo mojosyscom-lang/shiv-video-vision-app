6f9ab243086f1c15ce9555c7a8fa76fe54220127
mojosyscom-lang <mojosyscom@gmail.com>
2026-02-04 01:25:36 +0530

    Clean up api.js by removing blank lines
    Removed unnecessary blank lines in api.js

--- js/api.js
+++ js/api.js
@@ -1,8 +1,6 @@
 const API = "https://script.google.com/macros/s/AKfycbwaWc2LJ2vzCRzJlbpYRiQ58b555JR7-s2TscDD9pSz6P7SyVzpz5t2MOmtf7u62pia/exec";
 
 let _redirectingToLogin = false;
-
-
 async function api(data) {
   // Attach session without overwriting business fields
   if (data.action !== "login") {
