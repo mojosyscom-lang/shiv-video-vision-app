6dc5d554b1b64143b1659ab06415e2e3db24390d
mojosyscom-lang <mojosyscom@gmail.com>
2026-02-03 15:33:51 +0530

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,5 +1,4 @@
 const API="https://script.google.com/macros/s/AKfycbwaWc2LJ2vzCRzJlbpYRiQ58b555JR7-s2TscDD9pSz6P7SyVzpz5t2MOmtf7u62pia/exec";
-
 async function api(data){
   const r=await fetch(API,{method:"POST",body:JSON.stringify(data)});
   return r.json();
