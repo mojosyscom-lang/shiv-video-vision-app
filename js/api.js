ce1fc2dfaebbdfb63d074b40a6d70744dce48559
mojosyscom-lang <mojosyscom@gmail.com>
2026-02-04 02:19:06 +0530

    Update API endpoint URL in api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API = "https://script.google.com/macros/s/AKfycbwaWc2LJ2vzCRzJlbpYRiQ58b555JR7-s2TscDD9pSz6P7SyVzpz5t2MOmtf7u62pia/exec";
+const API = "https://script.google.com/macros/s/AKfycbwbvf0x6EJpxLZ9hNtnMgPFQhpJgfeM4L3f0XLGeEE0BvOKbBTcbDjGKVdL10Ma_fmQ/exec";
 
 let _redirectingToLogin = false;
 
