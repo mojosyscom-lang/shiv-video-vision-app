3dba43ff0736a563ea40b11c66f7a26b68cff876
mojosyscom-lang <mojosyscom@gmail.com>
2026-02-06 06:15:09 +0530

    Update nozoom.js

--- js/nozoom.js
+++ js/nozoom.js
@@ -20,15 +20,4 @@
 })();
 
 
-function toggleTheme() {
-  document.body.classList.toggle("dark");
-  localStorage.setItem(
-    "theme",
-    document.body.classList.contains("dark") ? "dark" : "light"
-  );
-}
-document.addEventListener("DOMContentLoaded", () => {
-  if (localStorage.getItem("theme") === "dark") {
-    document.body.classList.add("dark");
-  }
-});
+
